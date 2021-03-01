fb716cae7d0fc906037c41d8a65e85b872e7399e
Jordan Kelly <profjjk@gmail.com>
2021-03-01 14:17:55 -0800

    initial calendar page structure

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 function Calendar() {
   return (
-    <div>
-
+    <div className="container-fluid">
+      <header className="row">
+        <div className="col-8 border">
+          Announcement Area
+        </div>
+        <div className="col-4 border">
+          Mini-calendar area
+        </div>
+      </header>
+      <main className="row">
+        <div className="col-12 border">
+          Event list area
+        </div>
+      </main>
     </div>
   )
 }
