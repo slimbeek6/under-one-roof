102504c364555063cf1501760e7ed803803344c2
Jordan Kelly <profjjk@gmail.com>
2021-03-01 14:37:49 -0800

    create event api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,65 @@ const router = require("express").Router();
 // router.route("/route")
 // .get(controller.action);
 
+// EVENTS
+// =============================================================
+// Get all events, include the host's name.
+router.route("/api/event", (req, res) => {
+  db.Event.findAll({
+    include: [
+      {
+        model: db.User,
+        as: "host",
+        attributes: ["firstname"]
+      }
+    ],
+  }).then(event => {
+      res.json(event)
+  }).catch(err => {
+      console.error(err)
+      res.send(false)
+  })
+})
 
+// Post new event.
+router.route("/api/event", (req, res) => {
+  db.Event.create(req.body)
+  .then(eventData => {
+      db.UserEvent.create({
+        UserId: req.body.hostId,
+        EventId: eventData.id
+      }).then(() => {
+        res.send(true)
+      }).catch(err => {
+        console.error(err);
+        res.send(false);
+      })
+  }).catch(err => {
+      console.error(err)
+      res.send(false)
+  })
+})
+// route used to get 
+router.route("/api/event/:id", (req, res) => {
+  db.Event.update(req.body, { where: { id: req.params.id } })
+    .then(() => {
+      res.send(true);
+    }).catch(err => {
+      console.log(err);
+      res.send(false);
+    })
+})
+
+// route used to delete an event
+router.route("/api/event/:id", (req, res) => {
+  db.Event.destroy({ where: { id: req.params.id } })
+    .then(() => {
+      res.send(true)
+    }).catch(err => {
+      console.log(err);
+      res.send(false);
+    })
+})
 
 
 // send react app
