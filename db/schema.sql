58c8a6f49493cedd7dd260cf2189d4777554c460
Jordan Kelly <profjjk@gmail.com>
2021-03-01 12:52:39 -0800

    update package

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,3 @@
 DROP DATABASE IF EXISTS household;
 CREATE DATABASE household;
 
-USE household;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,9 @@
 // Dependencies
 // =============================================================
 var db = require("../models");
-const path = require("path");
-const router = require("express").Router();
 
-// API Routes
-router.route("/route")
-.get(controller.action);
-
-
-
-
-// send react app
-// we need to send the client the compiled index.html file
-if (process.env.NODE_ENV === "production") {
-    router.use(function (req, res) {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"))
-    })
-}
+// Routes
+// =============================================================
+module.exports = function(app) {
   
-module.exports = router;
+};
