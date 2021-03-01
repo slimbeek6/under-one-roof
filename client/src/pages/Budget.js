edf4078e8fa0990cdc3b66e86712ef9cb74fba22
Shaun Limbeek <soccercomet@yahoo.com>
2021-03-01 14:49:49 -0800

    commented out sections that aren't working yet. Added headers to largest expense table

--- client/src/pages/Budget.js
+++ client/src/pages/Budget.js
@@ -6,39 +6,32 @@ import { ADD_EXPENSE, UPDATE_EXPENSE, DELETE_EXPENSE, GET_EXPENSES } from "../ut
 import "./style.css";
 
 const Budget = () => {
-    const expnameRef = useRef();
-    const expamtRef = useRef();
-    const exptypeRef = useRef();
-    const [state, dispatch] = useStoreContext();
+    // const expnameRef = useRef();
+    // const expamtRef = useRef();
+    // const exptypeRef = useRef();
+    // const [state, dispatch] = useStoreContext();
 
-    const newExpense = {
-        expenseName: expnameRef.current.value,
-        expenseAmount: expamtRef.current.value,
-        expenseType: exptypeRef.current.value,
-        expenseDate: Date.now()
-    }
+    // const newExpense = {
+    //     expenseName: expnameRef.current.value,
+    //     expenseAmount: expamtRef.current.value,
+    //     expenseType: exptypeRef.current.value,
+    //     expenseDate: Date.now()
+    // }
 
-    const getBudget = () => {
-        dispatch({ type: GET_EXPENSES })
-    };
+    // const getBudget = () => {
+    //     dispatch({ type: GET_EXPENSES })
+    // };
 
-    useEffect(() =>{
-        getBudget();
-    }, []);
+    // useEffect(() =>{
+    //     getBudget();
+    // }, []);
 
-    const addExpense = () => {
-        dispatch({
-            type: ADD_EXPENSE,
-            expense: newExpense
-        });
-    };
-
-    const removeExpense = (id) => {
-        dispatch({
-            type: DELETE_EXPENSE,
-            _id: state.currentExpense._id
-        });
-    };
+    // const addExpense = () => {
+    //     dispatch({
+    //         type: ADD_EXPENSE,
+    //         expense: newExpense
+    //     });
+    // };
 
     return (
             <div className="container-fluid">
@@ -50,11 +43,11 @@ const Budget = () => {
                 <div className="row">
                     <div className="card col-md-5">
                         <h2>Total Budget</h2>
-                        <p><strong>Pie Chart Here</strong></p>
+                        <p><strong>Pie Chart Here populated by GlobalState</strong></p>
                     </div>
                     <div className="card col-md-5">
                         <h2>Roommate Budget and Payments</h2>
-                        <p><strong>Bar Chart Here</strong></p>
+                        <p><strong>Bar Chart Here populated by GlobalState</strong></p>
                     </div>
                 </div>
                 <div className="row">
@@ -63,17 +56,26 @@ const Budget = () => {
                         <div className="row">
                             <div className="col-md-6">
                                 <h3>Add New Expense:</h3>
-                                <form className="form-group" onSubmit={addExpense}>
+                                {/* <form className="form-group" onSubmit={addExpense}>
                                     <input className="form-control mb-5" required ref={expnameRef} placeholder="Name of Expense"/>
                                     <input className="form-control mb-5" required ref={expamtRef} placeholder="Expense Amount" />
                                     <input className="form-control mb-5" required ref={exptypeRef} placeholder="Expense Type, enter Rent, Utilities, or Other" />
                                     <button className="btn btn-success mt-3 mb-5" type="submit">Save Expense</button>
-                                </form>
+                                </form> */}
                             </div>
                             <div className="col-md-6">
-                                <h3>Largest Other Expenses</h3>
+                                <h3>Largest Other Expenses:</h3>
                                 <table>
-                                        Table
+                                    <tr>
+                                        <th>Expense Name</th>
+                                        <th>Expense Amount</th>
+                                        <th>Expense Submitted By</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Rent</td>
+                                        <td>$1000</td>
+                                        <td>Dave</td>
+                                    </tr>    
                                 </table>                       
                             </div>
                         </div>
