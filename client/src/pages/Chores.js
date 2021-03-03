2abddb5062f93aed92242e974c3c0c902a983135
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-03 14:06:26 -0800

    Updating Chores.js pages with getChores and addChore methods

--- client/src/pages/Chores.js
+++ client/src/pages/Chores.js
@@ -2,50 +2,71 @@ import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import API from "../utils/API";
 import { useStoreContext } from "../utils/GlobalState";
-import { ADD_CHORE, UPDATE_CHORE, DELETE_CHORE, GET_CHORES } from "../utils/actions";
+// import { ADD_CHORE, UPDATE_CHORE, DELETE_CHORE, GET_CHORES } from "../utils/actions";
 import "./style.css";
 
 const Chores = () => {
-//     const choreNameRef = useRef();
-//     const choreDescRef = useRef();
-//     const choreFreqRef = useRef();
-//     const [state, dispatch] = useStoreContext();
+    const choreNameRef = useRef();
+    const choreDescRef = useRef();
+    const choreFreqRef = useRef();
 
-//     const newChore = {
-//         choreName: choreNameRef.current.value,
-//         choreDescription: choreDescRef.current.value,
-//         choreFrequency: choreFreqRef.current.value
-//     }
+    const choreList = [];
+
+    const getChores = () => {
+        API.getChores()
+        .then(results => {
+            choreList.push(results);
+            console.log(choreList);
+        });
+    };
+
+    getChores();
+
+    const addChore = () => {
+        let newChore = {
+            choreName: choreNameRef.current.value,
+            choreDescription: choreDescRef.current.value,
+            choreFrequency: choreFreqRef.current.value
+        }
+        console.log(newChore);
+        API.addChore(newChore);
+    };
+
+    // const newChore = {
+    //     choreName: choreNameRef.current.value,
+    //     choreDescription: choreDescRef.current.value,
+    //     choreFrequency: choreFreqRef.current.value
+    // }
 
 
-//     const getChores = () => {
-//         dispatch({ type: GET_CHORES })
-//     };
+    // const getChores = () => {
+    //     dispatch({ type: GET_CHORES })
+    // };
 
-//     useEffect(() => {
-//         getChores();
-//     }, []);
+    // useEffect(() => {
+    //     getChores();
+    // }, []);
 
-//     const addChore = () => {
-//         dispatch({
-//             type: ADD_CHORE,
-//             chore: newChore
-//         });
-//     };
+    // const addChore = () => {
+    //     dispatch({
+    //         type: ADD_CHORE,
+    //         chore: newChore
+    //     });
+    // };
 
-//     const removeChore = () => {
-//         dispatch({
-//             type: DELETE_CHORE,
-//             _id: state.currentChore._id
-//         });
-//     };
+    // const removeChore = () => {
+    //     dispatch({
+    //         type: DELETE_CHORE,
+    //         _id: state.currentChore._id
+    //     });
+    // };
 
-//     const updateChore = () => {
-//         dispatch({
-//             type: UPDATE_CHORE,
-//             _id: state.currentChore._id
-//         });
-//     };
+    // const updateChore = () => {
+    //     dispatch({
+    //         type: UPDATE_CHORE,
+    //         _id: state.currentChore._id
+    //     });
+    // };
 
     return (
         <div>
