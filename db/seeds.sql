c18c37eb5c63d3fe889e69457e2d178bc5ff17d4
Shaun Limbeek <soccercomet@yahoo.com>
2021-03-03 12:23:02 -0800

    adding expense seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,7 @@
 use household;
 
+INSERT INTO household.expenses (expenseName, expenseAmount, expenseDate, expenseType, createdAt, updatedAt, paid) 
+VALUES ("electricity", 100, "2021-02-02 19:54:05", "utilities", "2021-02-02 19:54:05", "2021-02-02 19:54:05", false);
+
+INSERT INTO household.expenses (expenseName, expenseAmount, expenseDate, expenseType, createdAt, updatedAt, paid) 
+VALUES ("cable", 100, "2021-02-02 19:54:05", "utilities", "2021-02-02 19:54:05", "2021-02-02 19:54:05", false);
