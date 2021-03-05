8229a7f3b2bad62f34b6dfd0c60a96459a64b8ee
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-04 22:59:28 -0800

    Adding chore controller, clean up api routes formatting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,13 +13,15 @@ router
   .post(expenseController.add)
   .get(expenseController.findAll);
 
-  router.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+
+// Authentication routes
+router.use(function(req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
 
 router
   .route("/api/auth/signup")
