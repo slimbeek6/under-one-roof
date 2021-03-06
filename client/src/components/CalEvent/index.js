67a62e0af4a5d870c2e5b01c1c7b2e4d551c109f
Jordan Kelly <profjjk@gmail.com>
2021-03-06 15:45:37 -0800

    delete event

--- client/src/components/CalEvent/index.js
+++ client/src/components/CalEvent/index.js
@@ -15,13 +15,22 @@ function CalEvent() {
       }).catch(err => console.error(err))
   }, [])
 
+  const handleDeleteBtn = event => {
+    let id = event.target.getAttribute('data-id');
+    console.log(id);
+    API.deleteEvent(id)
+      .then(res => {
+        console.log("Event deleted!")
+      }).catch(err => console.error(err))
+  }
+
   return (
     events.map(event => (
-      <div className="row border p-3 m-0">
+      <div className="row border p-3 m-0" key={event.id}>
         <h3 className="col-10"><DayJS element='span' format='MMM D'>{event.date}</DayJS>: {event.title}</h3>
         <div className="col-2">
-          <button className="btn edit">Edit</button>
-          <button className="btn delete ml-3">Delete</button>
+          <button className="btn edit" data-id={event.id}>Edit</button>
+          <button className="btn delete ml-3" onClick={handleDeleteBtn} data-id={event.id}>Delete</button>
         </div>
       </div>
     ))
