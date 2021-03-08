45dececaefe14028cf78727e144d3b5b835615a7
Jordan Kelly <profjjk@gmail.com>
2021-03-08 14:44:59 -0800

    update event model and remove association in user model

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MiniCal from '../components/MiniCal';
 import CalEvent from '../components/CalEvent';
+import EventForm from '../components/EventForm';
 
 function Calendar() {
   const [displayForm, setDisplayForm] = useState(false);
@@ -25,8 +26,8 @@ function Calendar() {
       </header>
       <main className="row">
         <div className="col-12 p-3">
-          <button className="btn btn-primary m-0 mb-3">Add Event</button>
-          <CalEvent />
+          <button className="btn btn-primary m-0 mb-3" onClick={showForm}>Add Event</button>
+          {displayForm ? <EventForm hideForm={hideForm} /> : <CalEvent />}
         </div>
       </main>
     </div>
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const routes = require("./routes/apiRoutes.js");
 app.use(routes);
 
 
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
