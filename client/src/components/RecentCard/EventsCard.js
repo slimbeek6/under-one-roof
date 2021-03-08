0c12d1aae377f8dbf199e8a6ab4842d812023825
Jordan Kelly <profjjk@gmail.com>
2021-03-08 11:32:57 -0800

    style RecentCards

--- client/src/components/RecentCard/EventsCard.js
+++ client/src/components/RecentCard/EventsCard.js
@@ -4,21 +4,21 @@ import './index.css';
 function EventsCard(props) {
 
   return (
-  <div className="row">
-    <div className="col-12 text-center border">
-      {props.heading}
-    </div>
-    <div className="px-1 border">
+    <div>
+      <div className="row border">
+        <div className="col-12 listHeader text-center border">
+          {props.heading}
+        </div>
+      </div>
       {props.list.map(listItem => {
         return (
-          <div className="row listItem border" key={listItem.id}>
-            <p className="col-4 date">{listItem.eventDate.slice(5)}</p>
-            <p className="col-8 name">{listItem.eventName}</p>
+          <div className="row listItem border px-1" key={listItem.id}>
+            <p className="col-3 purple">{listItem.eventDate.slice(5)}</p>
+            <p className="col-9">{listItem.eventName}</p>
           </div>
         )
       })}
     </div>
-  </div>
   )
 }
 
