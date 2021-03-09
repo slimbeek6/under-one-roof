698ef8be48f1ba15f66ba95f4107e1d45ac7eec3
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-08 16:14:33 -0800

    Fixed chore controller and routes

--- client/src/pages/Chores.js
+++ client/src/pages/Chores.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import API from "../utils/API";
-import { useChoreContext } from "../utils/GlobalState";
+// import { useChoreContext } from "../utils/GlobalState";
 // import { ADD_CHORE, UPDATE_CHORE, DELETE_CHORE, GET_CHORES } from "../utils/actions";
 import "./style.css";
 
