fd3527708424252e41a51901fce505bef80e2e83
Jordan Kelly <profjjk@gmail.com>
2021-03-08 20:55:45 -0800

    added some style to the landing page and profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,30 @@
 import React from "react";
 import AuthService from "../services/auth.service";
 
+const style = {
+  logo: {
+    height: '200px',
+    objectFit: 'contain'
+  }
+}
+
 const Profile = () => {
   const currentUser = AuthService.getCurrentUser();
 
   return (
     <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>{currentUser.username}</strong> Profile
-        </h3>
+      <header className="jumbotron yellowBG">
+        <div className="row">
+          <div className="col-4 d-flex">
+            <img className="mx-auto" src="/assets/img/Brand/UnderOneRoof.png" style={style.logo} />
+          </div>
+          <div className="col-8 pt-5">
+            <h4>Welcome home!</h4>
+            <h2 className="display-3">588 Lombard St.</h2>
+          </div>
+        </div>
       </header>
-      <p>
+      {/* <p>
         <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
         {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
       </p>
@@ -25,7 +38,30 @@ const Profile = () => {
       <ul>
         {currentUser.roles &&
           currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-      </ul>
+      </ul> */}
+      <main className="row justify-content-center">
+        <h2 className="col-12 text-center mt-4 display-4 purple bold">Who are you?</h2>
+
+        <div className="col-3 card mx-3">
+          <img className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+          <h2 className="text-center mt-3">Shaun</h2>
+        </div>
+
+        <div className="col-3 card mx-3">
+          <img className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+          <h2 className="text-center mt-3">Ryan</h2>
+        </div>
+
+        <div className="col-3 card mx-3">
+          <img className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+          <h2 className="text-center mt-3">Jordan</h2>
+        </div>
+
+        <div className="col-3 card mx-3">
+          <img className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+          <h2 className="text-center mt-3">Add New Roommate</h2>
+        </div>
+      </main>
     </div>
   );
 };
