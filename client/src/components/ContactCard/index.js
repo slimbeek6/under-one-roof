5f881b9828d25d3abc9b6e142a24f492d7ad7333
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-10 11:13:32 -0800

    Getting ContactCards to render on Roommate Contact List section

--- client/src/components/ContactCard/index.js
+++ client/src/components/ContactCard/index.js
@@ -4,19 +4,19 @@ function ContactCard(props) {
     return (
         <>
             <h2 className="col-12 large text-center mt-4 display-4 blue bold">Roommate Contact List</h2>
-            {props.users.map(user => {
+            {props.list.map(list => {
                 return (
-                    <div className="col-lg-3 col-md-4 card mx-3" key={user.id}>
-                        <div className="blueBG">
+                    <div className="col-lg-3 col-md-4 card mx-3" key={list.id}>
+                        <div className="blueBG white">
                             <img className="img-fluid profile-img-card" src="/assets/img/Profile/profileICON-96.png" />
-                            <h2 className="medium text-center mt-3">{user.userName}</h2>
+                            <h2 className="medium text-center mt-3">{list.userName}</h2>
                         </div>
-                            <p className="medium text-center mt-3">{user.contactPhone}</p>
-                            <p className="medium text-center mt-3">{user.contactEmail}</p>
-                            <p className="medium text-center mt-3"><em>Emergency Contact:</em></p>
-                            <p className="medium text-center mt-3">{user.emergencyName}</p>
-                            <p className="medium text-center mt-3">{user.emergencyRelationship}</p>
-                            <p className="medium text-center mt-3">{user.emergencyPhone}</p>
+                            <p className="medium text-center mt-3">{list.contactPhone}</p>
+                            <p className="medium text-center mt-3">{list.contactEmail}</p>
+                            <p className="medium text-center mt-3"><strong>Emergency Contact:</strong></p>
+                            <p className="medium text-center mt-3">{list.emergencyName}</p>
+                            <p className="medium text-center mt-3">{list.emergencyRelationship}</p>
+                            <p className="medium text-center mt-3">{list.emergencyPhone}</p>
                     </div>
                 )
             })}
