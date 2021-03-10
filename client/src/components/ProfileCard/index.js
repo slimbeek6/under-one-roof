a7d7745f7cb63bc8eb682e41f4ae9b302f355bc9
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-10 03:50:55 -0800

    Adding more styling to Profile Page, adjusting profile card

--- client/src/components/ProfileCard/index.js
+++ client/src/components/ProfileCard/index.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 function ProfileCard(props) {
   return (
     <>
-      <h2 className="col-12 text-center mt-4 display-4 purple bold">Who are you?</h2>
+      {/* <h2 className="col-12 text-center mt-4 display-4 purple bold">Who are you?</h2> */}
       {props.users.map(user => {
         return (
-          <Link className="col-3 card mx-3 imitate-btn" to="/landing" key={user.id}>
+          <Link className="col-sm-8 col-md-5 col-lg-3 card mx-3 imitate-btn" to="/landing" key={user.id}>
             <img className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
             <h2 className="text-center mt-3">{user.userName}</h2>
           </Link>
         )
       })}
-      <div className="col-3 card mx-3 imitate-btn" onClick={props.showForm}>
+      <div className="col-sm-8 col-md-5 col-lg-3 card mx-3 imitate-btn" onClick={props.showForm}>
         <img className="profile-img-card" src="/assets/img/addUser/addUserICON-96.png" />
         <h2 className="text-center mt-3">Add New Roommate</h2>
       </div>
